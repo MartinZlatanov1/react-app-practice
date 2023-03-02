36812f952ef0384cbaee58791117d1cb326fe9c8
Bogdan Bogdanov <bbogdanov@vmware.com>
2023-03-02 16:44:23 +0200

    Simple todo application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FormCreator } from './components/todo-creator';
+import { TodoItem } from './components/todo-item/todo-item';
+import json from './todos.json';
 
 function App() {
+  let [todos, addTodos] = useState(json);
+
+
+  const addTodo = (title) => {
+    addTodos([...todos, { title, isDone: false }])
+
+    console.log(todos);
+  }
+
+  const removeTodo = (index) => {
+    return () => {
+      const tds = todos.filter((todo, idx) => index !== idx);
+      addTodos(tds);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo app</h1>
+      <hr />
+
+      <FormCreator createTodo={addTodo} />
+
+      {
+        todos.map((todo, index) => {
+          return (
+            <TodoItem key={index} itemIndex={index} removeItem={removeTodo(index)} title={todo.title} checkItem={() => { }} />
+          );
+        })
+      }
+
     </div>
   );
 }
