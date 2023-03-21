d715b22e82fbec2d54a8765ca8ea19d91a78ca45
MartinZlatanov1 <martinzlatanov2004@gmail.com>
2023-03-21 17:25:35 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -5,38 +5,53 @@ import { TodoItem } from './components/todo-item/todo-item';
 import json from './todos.json';
 
 function App() {
-  let [todos, addTodos] = useState(json);
+let [todos, setTodos] = useState(json);
+
+const addTodo = (title) => {
+setTodos([...todos, { title, isDone: false }])
+}
+
+const removeTodo = (index) => {
+const filteredTodos = todos.filter((todo, idx) => index !== idx);
+setTodos(filteredTodos);
+};
+
+const checkTodo = (index) => {
+const newTodos = [...todos];
+newTodos[index].isDone = !newTodos[index].isDone;
+setTodos(newTodos);
+}
+
+const editTodo = (index, newTitle) => {
+const newTodos = [...todos];
+newTodos[index].title = newTitle;
+setTodos(newTodos);
+}
 
 
-  const addTodo = (title) => {
-    addTodos([...todos, { title, isDone: false }])
+return (
+<div className="App">
+<h1>Todo app</h1>
+<hr />
 
-    console.log(todos);
-  }
+<FormCreator createTodo={addTodo} />
 
-  const removeTodo = (index) => {
-    return () => {
-      const tds = todos.filter((todo, idx) => index !== idx);
-      addTodos(tds);
-    }
-  };
+  <body>
+    {todos.map((todo, index) => (
+      <TodoItem
+        key={index}
+        itemIndex={index}
+        title={todo.title}
+        isDone={todo.isDone}
+        checkItem={() => checkTodo(index)}
+        removeItem={() => removeTodo(index)}
+        editItem={(newTitle) => editTodo(index, newTitle)}
+      />
+    ))}
+  </body>
 
-  return (
-    <div className="App">
-      <h1>Todo app</h1>
-      <hr />
+</div>
 
-      <FormCreator createTodo={addTodo} />
-
-      {
-        todos.map((todo, index) => {
-          return (
-            <TodoItem key={index} itemIndex={index} removeItem={removeTodo(index)} title={todo.title} checkItem={() => { }} />
-          );
-        })
-      }
-
-    </div>
   );
 }
 
--- src/components/todo-item/todo-item.jsx
+++ src/components/todo-item/todo-item.jsx
@@ -1,11 +1,12 @@
 import './todo-item.css'
 
-export function TodoItem({ title, checkItem, removeItem }) {
+export function TodoItem({ title, checkTodo, removeItem, editTodo }) {
+
 
   return (
     <div className="todo__item">
-      <input type="checkbox" onChange={checkItem} />
-      { title }
+      <input type="checkbox" checked={checkTodo} onChange={checkTodo} />
+      <a className="todo__item__title" contentEditable onBlur={({ target }) => editTodo(target.textContent)}>{title}</a>
       <button onClick={removeItem}>X</button>
     </div>
   )
